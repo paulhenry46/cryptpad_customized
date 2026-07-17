1dae0db81661ef8ad30b14764e1a7ac3d7874c4d
Paulhenry Saux <paulhenrysaux@gmail.com>
2026-07-17 15:39:17 +0200

    add application_config.js file

---
+++ customize/application_config.js
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2023 XWiki CryptPad Team <contact@cryptpad.org> and contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/*
+ * You can override the configurable values from this file.
+ * The recommended method is to make a copy of this file (/customize.dist/application_config.js)
+   in a 'customize' directory (/customize/application_config.js).
+ * If you want to check all the configurable values, you can open the internal configuration file
+   but you should not change it directly (/common/application_config_internal.js)
+*/
+
+(() => {
+const factory = (AppConfig) => {
+    // Example: If you want to remove the survey link in the menu:
+    // AppConfig.surveyURL = "";
+    AppConfig.registeredOnlyTypes = ['file', 'contacts', 'notifications', 'support','drive', 'teams', 'sheet', 'doc', 'presentation', 'pad', 'kanban', 'code', 'form', 'poll', 'whiteboard', 'slide', 'convert', 'diagram'];
+    // To inform users of the support ticket panel which languages your admins speak:
+    //AppConfig.supportLanguages = [ 'en', 'fr' ];
+    AppConfig.disableAnonymousPadCreation = true;
+    
+    return AppConfig;
+};
+
+
+// Do not change code below
+if (typeof(module) !== 'undefined' && module.exports) {
+    module.exports = factory(
+        require('../www/common/application_config_internal.js')
+    );
+} else if ((typeof(define) !== 'undefined' && define !== null) && (define.amd !== null)) {
+    define(['/common/application_config_internal.js'], factory);
+}
+
+})();
